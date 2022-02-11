912e7112fce5d0cf334dd87b3adb4c39f894eab5
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2022-02-11 14:17:53 -0800

    prepare deloy

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider('ill domain ordinary evidence accident neutral scout nation coyote potato island risk', 'https://rinkeby.infura.io/v3/cef4d0f52f754a47a01bd4d0f932b25d')
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+
+    const account = await web3.eth.getAccounts();
+
+    console.log('attemp to deloy', account[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)).deploy({ data: bytecode }).send({ gas: '1000000', from: account[0] })
+
+
+    console.log("Address", result.options.address);
+
+    provider.engine.stop()
+}
+
+
+deploy();
